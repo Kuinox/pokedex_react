a7d0ba765cfd89fdfc16cc0a80c8c081ab7970dc
Nicolas Vandeginste <nicolas.vandeginste@signature.one>
2023-08-09 19:05:51 +0200

    Fixed bad import.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { initializePokemonStore } from "@kuinox/pokedex.js";
 const root = ReactDOM.createRoot(document.getElementById('root'));
